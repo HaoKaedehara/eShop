1ce20df11c91fb3f797d99bfec1699a7d84f12ff
MSI\Chi Hao Le Nguyen <haoviettravinh2@gmail.com>
2022-08-24 21:24:18 +0700

    111

---
+++ eShop/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using eShop.Database;
+
+namespace eShop.Areas.Admin.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected readonly AppDbContext _db;
+
+        public BaseController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+    }
+}
---
+++ eShop/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using eShop.Database;
+
+namespace eShop.Areas.Admin.Controllers
+{
+    public class CategoryController : BaseController
+    {
+
+
+        public CategoryController(AppDbContext db) : base(db)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            var data = _db.ProductCategories.OrderByDescending(c=>c.Id).ToList();
+            return View(data);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
